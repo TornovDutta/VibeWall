1bfeea457e982ed0a7594480bc1eb18a959e3655
TornovDutta <tornovdutta@gmail.com>
2025-10-14 13:12:43 +0530

    initial commit

---
+++ src/main/java/org/example/vibewall/VIbeWallApplication.java
@@ -0,0 +1,13 @@
+package org.example.vibewall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VIbeWallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VIbeWallApplication.class, args);
+    }
+
+}
