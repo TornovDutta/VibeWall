aeeb5c9d166a79ad0014bb698a30512c41c0af7e
TornovDutta <tornovdutta@gmail.com>
2025-10-14 22:18:21 +0530

    confession model add

---
+++ src/main/java/org/example/vibewall/model/Confession.java
@@ -0,0 +1,26 @@
+package org.example.vibewall.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collation = "Confession")
+@Data
+public class Confession {
+    @Id
+    private Integer id;
+    private String content;
+    @CreatedDate
+    private Date time;
+
+    public Confession(){
+        this.time=new Date();
+    }
+    public Confession(String content){
+        this.content=content;
+        this.time=new Date();
+    }
+}
