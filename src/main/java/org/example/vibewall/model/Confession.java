8a6400e64f434e6ca8a478ef5d87216da54f5335
TornovDutta <tornovdutta@gmail.com>
2025-10-15 13:02:18 +0530

    change the primary key datatype

--- src/main/java/org/example/vibewall/model/Confession.java
+++ src/main/java/org/example/vibewall/model/Confession.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @Data
 public class Confession {
     @Id
-    private Integer id;
+    private String id;
     private String content;
     @CreatedDate
     private Date time;
