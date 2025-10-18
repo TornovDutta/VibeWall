78f5cd7d937163b38ef7460f12dda7b3dba9d10f
TornovDutta <tornovdutta@gmail.com>
2025-10-19 02:52:20 +0530

    enable secuirty

--- src/main/java/org/example/vibewall/config/SecurityConfig.java
+++ src/main/java/org/example/vibewall/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package org.example.vibewall.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain
 }
