1a01a40e1269dd8cc886fcd834f0f203b6e37b80
TornovDutta <tornovdutta@gmail.com>
2025-10-18 17:21:52 +0530

    add the repo layer to the users

---
+++ src/main/java/org/example/vibewall/DAO/usersRepo.java
@@ -0,0 +1,9 @@
+package org.example.vibewall.DAO;
+
+import org.example.vibewall.model.Users;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface usersRepo extends MongoRepository<Users,String> {
+}
