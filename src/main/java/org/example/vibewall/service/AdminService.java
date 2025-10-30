f98a784857013c23b25f33b50c690beb33fd07b9
TornovDutta <tornovdutta@gmail.com>
2025-10-30 22:14:11 +0530

    add logger

--- src/main/java/org/example/vibewall/service/AdminService.java
+++ src/main/java/org/example/vibewall/service/AdminService.java
@@ -6,16 +6,20 @@ import org.example.vibewall.exception.AdminNotFoundException;
 import org.example.vibewall.exception.ReportNotFoundException;
 import org.example.vibewall.model.Report;
 import org.example.vibewall.model.Users;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 @Service
 public class AdminService {
     private final UsersRepo repo;
     private final ReportRepo reportRepo;
     private final PasswordEncoder passwordEncoder;
+    private static final Logger logger= LoggerFactory.getLogger(AdminService.class);
 
     public AdminService(UsersRepo repo, PasswordEncoder passwordEncoder,ReportRepo reportRepo) {
         this.repo = repo;
@@ -31,6 +35,7 @@ public class AdminService {
         user.setUsername(passwordEncoder.encode(user.getUsername()));
         user.setRole("ADMIN");
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        logger.info("new admin add");
         return repo.save(user);
     }
 
@@ -40,14 +45,15 @@ public class AdminService {
 
         existingUser.setUsername(passwordEncoder.encode(user.getUsername()));
         existingUser.setPassword(passwordEncoder.encode(user.getPassword()));
-
+        logger.info("admin of id: "+id+" update");
         return repo.save(existingUser);
     }
 
     public void delete(String id) throws AdminNotFoundException{
-        Users existingUser = repo.findById(id)
+        repo.findById(id)
                 .orElseThrow(() -> new AdminNotFoundException("admin not found with id: " + id));
 
+        logger.info("admin of id: "+id+" remove");
         repo.removeById(id);
     }
 
@@ -71,6 +77,7 @@ public class AdminService {
     public Report reslove(String id, String status) throws ReportNotFoundException {
         Report report=reportRepo.findById(id).orElseThrow(()->
                 new ReportNotFoundException());
+        logger.info("id: "+id +" , reslove by admin");
         report.setStatus(status);
         return reportRepo.save(report);
     }
