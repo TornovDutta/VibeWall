262b4a14cf8198176549e42af60f104c09984d56
TornovDutta <tornovdutta@gmail.com>
2025-11-06 21:21:06 +0530

    enable security filter

--- src/main/java/org/example/vibewall/config/SecurityConfig.java
+++ src/main/java/org/example/vibewall/config/SecurityConfig.java
@@ -14,13 +14,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .formLogin(form -> form.disable())
-                .httpBasic(basic -> basic.disable());
+
         return http.build();
     }
 
